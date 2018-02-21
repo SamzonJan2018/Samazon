d52e63773840ccb7a41ba83e166ffb49a2eac3c8
jaguar2014 <emailmulat@gmail.com>
2018-02-21 11:05:35 -0500

    Created models and repositories

---
+++ src/main/java/com/example/demo/model/AppRole.java
@@ -0,0 +1,54 @@
+package com.example.demo.model;
+
+import org.hibernate.annotations.ManyToAny;
+import org.hibernate.validator.constraints.NotEmpty;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+public class AppRole {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+
+    @NotEmpty
+    @Column(unique = true)
+    private String roleName;
+
+    @ManyToMany(mappedBy = "roles")
+    private Set<AppUser> users;
+
+    public AppRole(){
+        this.users = new HashSet<>();
+    }
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    public Set<AppUser> getUsers() {
+        return users;
+    }
+
+    public void setUsers(Set<AppUser> users) {
+        this.users = users;
+    }
+
+
+}
---
+++ src/main/java/com/example/demo/model/Product.java
@@ -0,0 +1,65 @@
+package com.example.demo.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+
+    private String productName;
+
+    private String productPicture;
+
+    private Double productPrice;
+
+    private String productDescription;
+   // private int productQuantity;
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getProductPicture() {
+        return productPicture;
+    }
+
+    public void setProductPicture(String productPicture) {
+        this.productPicture = productPicture;
+    }
+
+    public Double getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(Double productPrice) {
+        this.productPrice = productPrice;
+    }
+
+    public String getProductDescription() {
+        return productDescription;
+    }
+
+    public void setProductDescription(String productDescription) {
+        this.productDescription = productDescription;
+    }
+}
